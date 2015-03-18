38284071db1cbcbf952415ea695e7f67b4d2aa82
Alister Fenix <a.fenix@icloud.com>
2015-03-18 15:48:13 -0700

    Add user model password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  validates_confirmation_of :password
+  before_save :encrypt_password
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def self.authentication(email, password)
+  end
 end
