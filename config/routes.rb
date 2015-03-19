a24d091ef5d3019b0a59eb89e9ae43c6c12c4d2a
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-03-18 18:48:29 -0700

    Add log-in functionality - but not create yet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
-  # get '/' => 'home#index' //Should be the same
+  # root :to => 'home#index'
+  get '/' => 'home#index'
+  get '/log-in' => "sessions#new"
+  post "/log-in" => "sessions#create"
+  get "/log-out" => "sessions#destroy", as: :log_out
 
   resources :users
 end
