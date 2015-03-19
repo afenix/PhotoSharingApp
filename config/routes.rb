c0938fa209d930384619627faf89568b9c44dd2b
Alister <a.fenix@icloud.com>
2015-03-19 12:10:21 -0700

    Add current_user method and display logged in user on home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # root :to => 'home#index'
   get '/' => 'home#index'
   get '/log-in' => "sessions#new"
-  post "/log-in" => "sessions#create"
-  get "/log-out" => "sessions#destroy", as: :log_out
+  post '/log-in' => "sessions#create"
+  get '/log-out' => "sessions#destroy", as: :log_out
 
   resources :users
 end
