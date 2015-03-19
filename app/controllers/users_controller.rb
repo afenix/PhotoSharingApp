fce2bc0947f8ef5e7372b495e0b27d42b85ec937
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-03-18 22:10:59 -0700

    Add login and logout capabilities as well as some css.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-def new
-  @user = User.new
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    flash[:notice] = "User created successfully!"
-    redirect_to "/"
-  else
-    flash[:alert] = "There was a problem, yo."
-    redirect_to :back
+  def new
+    @user = User.new
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User created successfully!"
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem, yo."
+      redirect_to :back
+    end
+  end
 
 private
   def user_params
